dea80ebc405a56319b760ddb3ab5d729f99d75c3
Niaz <niazulalam097@gmail.com>
2026-03-08 00:22:32 +0600

    get single category

--- dashboard/src/components/pages/UpdateCategory.jsx
+++ dashboard/src/components/pages/UpdateCategory.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import axios from "axios";
 
 const UpdateCategory = () => {
   const { id } = useParams();
+  const [getCategoryName, setCategoryName] = useState("");
+  const [getCategoryDes, setCategoryDes] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        `https://mern-ecommerce-91cv.onrender.com/api/v1/category/singlecategory/${id}`,
+      )
+      .then((res) => {
+        setCategoryName(res.data.data.name);
+        setCategoryDes(res.data.data.description);
+      });
+    console.log(getCategoryName);
+    console.log(getCategoryDes);
+  }, []);
+
+  
   return (
     <>
       <h3 className="font-bold">Update Category</h3>
@@ -14,11 +31,12 @@ const UpdateCategory = () => {
         <FieldGroup>
           <Field>
             <FieldLabel>Update Category Name</FieldLabel>
-            <Input placeholder="Update Category Name" />
+            <Input value={getCategoryName} placeholder="Update Category Name" />
           </Field>
           <Field>
             <FieldLabel>Update Category Description</FieldLabel>
             <Textarea
+              value={getCategoryDes}
               placeholder="Type your description here..."
               className={"resize-none"}
             />
