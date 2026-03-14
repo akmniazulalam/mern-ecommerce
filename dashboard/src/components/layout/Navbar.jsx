c21f4c125d1684a63b31420ae415d760cf99aac4
Niaz <niazulalam097@gmail.com>
2026-03-15 01:44:21 +0600

    Dark mode add

--- dashboard/src/components/layout/Navbar.jsx
+++ dashboard/src/components/layout/Navbar.jsx
@@ -1,24 +1,69 @@
-import React from 'react'
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { Bell } from "lucide-react"
+import React, { useState, useEffect } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Bell } from "lucide-react";
+import { Menu, Moon, Sun, X } from "lucide-react";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+
+  // Sync darkMode with html class on mount
+  useEffect(() => {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "dark") {
+    document.documentElement.classList.add("dark");
+    setDarkMode(true);
+  } else {
+    document.documentElement.classList.remove("dark");
+    setDarkMode(false);
+  }
+}, []);
+
+
+  const toggleDarkMode = () => {
+  setDarkMode((prev) => {
+    const newMode = !prev;
+
+    if (newMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+
+    return newMode;
+  });
+};
+
   return (
     <header className="h-16 border-b bg-background flex items-center justify-between px-6">
       <Input placeholder="Search..." className="max-w-sm" />
 
       <div className="flex items-center gap-4">
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={toggleDarkMode}
+          className="cursor-pointer">
+          {darkMode ? (
+            <Sun className="h-5 w-5 text-yellow-500" />
+          ) : (
+            <Moon className="h-5 w-5 text-muted-foreground" />
+          )}
+        </Button>
         <Button variant="ghost" size="icon">
           <Bell className="w-5 h-5" />
         </Button>
 
-        <div className="w-9 h-9 bg-primary rounded-full flex items-center justify-center text-white font-semibold">
+        <div className="w-9 h-9 bg-primary rounded-full flex items-center justify-center dark:text-black text-white font-semibold">
           NA
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
