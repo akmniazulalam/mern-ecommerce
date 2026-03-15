1f67f87f6c4d90148e4a35e423c31a46ef6845f7
Niaz <niazulalam097@gmail.com>
2026-03-16 02:53:39 +0600

    Update image

--- dashboard/src/components/pages/UpdateProduct.jsx
+++ dashboard/src/components/pages/UpdateProduct.jsx
@@ -8,24 +8,24 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const UpdateProduct = () => {
-    const { id } = useParams();
-  const [updateName, setUpdateName] = useState("")
-  const [updateDes, setUpdateDes] = useState("")
-  const [updateCategory, setUpdateCategory] = useState("")
-  const [updatePrice, setUpdatePrice] = useState("")
-  const [updateSize, setUpdateSize] = useState("")
-  const [updateColor, setUpdateColor] = useState("")
-  const [updateRam, setUpdateRam] = useState("")
-  const [updateStorage, setUpdateStorage] = useState("")
-  const [updateImage, setUpdateImage] = useState("")
-  const navigate = useNavigate()
+  const { id } = useParams();
+  const [updateName, setUpdateName] = useState("");
+  const [updateDes, setUpdateDes] = useState("");
+  const [updateCategory, setUpdateCategory] = useState("");
+  const [updatePrice, setUpdatePrice] = useState("");
+  const [updateSize, setUpdateSize] = useState("");
+  const [updateColor, setUpdateColor] = useState("");
+  const [updateRam, setUpdateRam] = useState("");
+  const [updateStorage, setUpdateStorage] = useState("");
+  const [updateImage, setUpdateImage] = useState("");
+  const [prevImage, setPrevImage] = useState("");
+  const navigate = useNavigate();
   useEffect(() => {
     axios
       .get(
-        `https://mern-ecommerce-91cv.onrender.com/api/v1/product/singleproduct/${id}`
+        `https://mern-ecommerce-91cv.onrender.com/api/v1/product/singleproduct/${id}`,
       )
       .then((res) => {
-        
         setUpdateName(res.data.data.name);
         setUpdateDes(res.data.data.description);
         setUpdateCategory(res.data.data.category);
@@ -34,90 +34,127 @@ const UpdateProduct = () => {
         setUpdateColor(res.data.data.color);
         setUpdateRam(res.data.data.ram);
         setUpdateStorage(res.data.data.storage);
-        setUpdateImage(res.data.data.image);
+        setPrevImage(res.data.data.image);
       });
   }, []);
 
   const handleUpdateProduct = () => {
-    const formData = new FormData()
-    formData.append("name", updateName)
-    formData.append("description", updateDes)
-    formData.append("category", updateCategory)
-    formData.append("price", updatePrice)
-    formData.append("size", updateSize)
-    formData.append("color", updateColor)
-    formData.append("ram", updateRam)
-    formData.append("storage", updateStorage)
-    if(updateImage){
-      formData.append("image", updateImage)
+    const formData = new FormData();
+    formData.append("name", updateName);
+    formData.append("description", updateDes);
+    formData.append("category", updateCategory);
+    formData.append("price", updatePrice);
+    formData.append("size", updateSize);
+    formData.append("color", updateColor);
+    formData.append("ram", updateRam);
+    formData.append("storage", updateStorage);
+    if (updateImage) {
+      formData.append("image", updateImage);
     }
-    axios.patch(`https://mern-ecommerce-91cv.onrender.com/api/v1/product/updateproduct/${id}`, formData)
-    toast.success("Successfully Updated")
-    setUpdateName("")
-    setUpdateDes("")
-    setUpdateCategory("")
-    setUpdatePrice("")
-    setUpdateSize("")
-    setUpdateColor("")
-    setUpdateRam("")
-    setUpdateStorage("")
-    setUpdateImage("")
+    axios.patch(
+      `https://mern-ecommerce-91cv.onrender.com/api/v1/product/updateproduct/${id}`,
+      formData,
+    );
+    toast.success("Successfully Updated");
+    setUpdateName("");
+    setUpdateDes("");
+    setUpdateCategory("");
+    setUpdatePrice("");
+    setUpdateSize("");
+    setUpdateColor("");
+    setUpdateRam("");
+    setUpdateStorage("");
+    setPrevImage("");
+    setUpdateImage("");
     setTimeout(() => {
-      navigate("/productlist")
+      navigate("/productlist");
     }, 1000);
-  }
+  };
   return (
     <>
-    <h3 className="font-bold">Update Product</h3>
-          <div className="max-w-1/3 mt-4">
-            <FieldGroup>
-              <Field>
-                <FieldLabel>Update Product Name</FieldLabel>
-                <Input value={updateName} onChange={(e)=> setUpdateName(e.target.value)} />
-              </Field>
-              <Field>
-                <FieldLabel>Update Product Description</FieldLabel>
-                <Textarea
-                  value={updateDes}
-                  className={"resize-none"}
-                  onChange={(e)=> setUpdateDes(e.target.value)}
-                />
-              </Field>
-              <Field>
-                <FieldLabel>Update Product Category</FieldLabel>
-                <Input value={updateCategory} onChange={(e)=> setUpdateCategory(e.target.value)} />
-              </Field>
-              <Field>
-                <FieldLabel>Update Product Price</FieldLabel>
-                <Input value={updatePrice} onChange={(e)=> setUpdatePrice(e.target.value)} />
-              </Field>
-              <Field>
-                <FieldLabel>Update Product Size</FieldLabel>
-                <Input value={updateSize} onChange={(e)=> setUpdateSize(e.target.value)} />
-              </Field>
-              <Field>
-                <FieldLabel>Update Product Color</FieldLabel>
-                <Input value={updateColor}  onChange={(e)=> setUpdateColor(e.target.value)} />
-              </Field>
-              <Field>
-                <FieldLabel>Update Product Ram</FieldLabel>
-                <Input value={updateRam} onChange={(e)=> setUpdateRam(e.target.value)} />
-              </Field>
-              <Field>
-                <FieldLabel>Update Product Storage</FieldLabel>
-                <Input value={updateStorage} onChange={(e)=> setUpdateStorage(e.target.value)} />
-              </Field>
-              <Field>
-                <FieldLabel>Update Product Image</FieldLabel>
-                <img src={updateImage} onChange={(e)=> setUpdateImage(e.target.files[0])} />
-              </Field>
-              <Field orientation="horizontal">
-                <Button onClick={handleUpdateProduct} className={"cursor-pointer"}>Update Product</Button>
-              </Field>
-            </FieldGroup>
-          </div>
+      <h3 className="font-bold">Update Product</h3>
+      <div className="max-w-1/3 mt-4">
+        <FieldGroup>
+          <Field>
+            <FieldLabel>Update Product Name</FieldLabel>
+            <Input
+              value={updateName}
+              onChange={(e) => setUpdateName(e.target.value)}
+            />
+          </Field>
+          <Field>
+            <FieldLabel>Update Product Description</FieldLabel>
+            <Textarea
+              value={updateDes}
+              className={"resize-none"}
+              onChange={(e) => setUpdateDes(e.target.value)}
+            />
+          </Field>
+          <Field>
+            <FieldLabel>Update Product Category</FieldLabel>
+            <Input
+              value={updateCategory}
+              onChange={(e) => setUpdateCategory(e.target.value)}
+            />
+          </Field>
+          <Field>
+            <FieldLabel>Update Product Price</FieldLabel>
+            <Input
+              value={updatePrice}
+              onChange={(e) => setUpdatePrice(e.target.value)}
+            />
+          </Field>
+          <Field>
+            <FieldLabel>Update Product Size</FieldLabel>
+            <Input
+              value={updateSize}
+              onChange={(e) => setUpdateSize(e.target.value)}
+            />
+          </Field>
+          <Field>
+            <FieldLabel>Update Product Color</FieldLabel>
+            <Input
+              value={updateColor}
+              onChange={(e) => setUpdateColor(e.target.value)}
+            />
+          </Field>
+          <Field>
+            <FieldLabel>Update Product Ram</FieldLabel>
+            <Input
+              value={updateRam}
+              onChange={(e) => setUpdateRam(e.target.value)}
+            />
+          </Field>
+          <Field>
+            <FieldLabel>Update Product Storage</FieldLabel>
+            <Input
+              value={updateStorage}
+              onChange={(e) => setUpdateStorage(e.target.value)}
+            />
+          </Field>
+          <Field>
+            <FieldLabel>Update Product Image</FieldLabel>
+            {prevImage && (
+              <img
+                src={prevImage}
+                alt="product"
+                className="h-20 mb-3 rounded"
+              />
+            )}
+            <input
+              type={"file"}
+              onChange={(e) => setUpdateImage(e.target.files[0])}
+            />
+          </Field>
+          <Field orientation="horizontal">
+            <Button onClick={handleUpdateProduct} className={"cursor-pointer"}>
+              Update Product
+            </Button>
+          </Field>
+        </FieldGroup>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default UpdateProduct
+export default UpdateProduct;
