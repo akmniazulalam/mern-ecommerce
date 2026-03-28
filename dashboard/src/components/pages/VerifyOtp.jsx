b7416ff169d8248f134974307e5074e3b1272f67
akmniazulalam <niazulalam097@gmail.com>
2026-03-28 14:04:48 +0600

    document title change using react-helmet-async

--- dashboard/src/components/pages/VerifyOtp.jsx
+++ dashboard/src/components/pages/VerifyOtp.jsx
@@ -2,29 +2,35 @@ import React from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Helmet } from "react-helmet-async";
 
 const VerifyOtp = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-muted">
-      <Card className="w-full max-w-md shadow-xl rounded-2xl text-center">
-        <CardHeader>
-          <CardTitle>Verify OTP</CardTitle>
-          <p className="text-sm text-muted-foreground">
-            Enter the OTP sent to email
-          </p>
-        </CardHeader>
+    <>
+      <Helmet>
+        <title>Otp Verify</title>
+      </Helmet>
+      <div className="flex items-center justify-center min-h-screen bg-muted">
+        <Card className="w-full max-w-md shadow-xl rounded-2xl text-center">
+          <CardHeader>
+            <CardTitle>Verify OTP</CardTitle>
+            <p className="text-sm text-muted-foreground">
+              Enter the OTP sent to email
+            </p>
+          </CardHeader>
 
-        <CardContent className="space-y-4">
-          <Input placeholder="Enter OTP" />
+          <CardContent className="space-y-4">
+            <Input placeholder="Enter OTP" />
 
-          <Button className="w-full cursor-pointer">Verify</Button>
+            <Button className="w-full cursor-pointer">Verify</Button>
 
-          <Button variant="ghost" className="w-full cursor-pointer">
-            Resend OTP
-          </Button>
-        </CardContent>
-      </Card>
-    </div>
+            <Button variant="ghost" className="w-full cursor-pointer">
+              Resend OTP
+            </Button>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 };
 
