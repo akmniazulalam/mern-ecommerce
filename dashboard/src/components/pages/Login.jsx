48908185c5987218345c0fd07319e346064750d4
akmniazulalam <niazulalam097@gmail.com>
2026-03-28 13:55:56 +0600

    document title change using react-helmet-async

--- dashboard/src/components/pages/Login.jsx
+++ dashboard/src/components/pages/Login.jsx
@@ -3,32 +3,40 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import { Helmet } from "react-helmet-async";
 
 const Login = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-muted w-full">
-      <Card className="w-full max-w-md shadow-xl rounded-2xl">
-        <CardHeader>
-          <CardTitle className="text-center text-2xl">Login to your account</CardTitle>
-        </CardHeader>
+    <>
+      <Helmet>
+        <title>Login</title>
+      </Helmet>
+      <div className="flex items-center justify-center min-h-screen bg-muted w-full">
+        <Card className="w-full max-w-md shadow-xl rounded-2xl">
+          <CardHeader>
+            <CardTitle className="text-center text-2xl">
+              Login to your account
+            </CardTitle>
+          </CardHeader>
 
-        <CardContent>
-          <form className="space-y-4">
-            <div>
-              <Label className={"mb-2"}>Email</Label>
-              <Input type="email" placeholder="Enter your email" />
-            </div>
+          <CardContent>
+            <form className="space-y-4">
+              <div>
+                <Label className={"mb-2"}>Email</Label>
+                <Input type="email" placeholder="Enter your email" />
+              </div>
 
-            <div>
-              <Label className={"mb-2"}>Password</Label>
-              <Input type="password" placeholder="**********" />
-            </div>
+              <div>
+                <Label className={"mb-2"}>Password</Label>
+                <Input type="password" placeholder="**********" />
+              </div>
 
-            <Button className="w-full mt-2 cursor-pointer">Login</Button>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
+              <Button className="w-full mt-2 cursor-pointer">Login</Button>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 };
 
