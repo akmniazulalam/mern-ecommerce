58b495d2c92296856c332d0eb848891240dd0596
akmniazulalam <niazulalam097@gmail.com>
2026-03-28 14:01:45 +0600

    document title change using react-helmet-async

--- dashboard/src/components/pages/Signup.jsx
+++ dashboard/src/components/pages/Signup.jsx
@@ -3,44 +3,53 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import { Helmet } from "react-helmet-async";
 
 const Signup = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-muted">
-      <Card className="w-full max-w-md shadow-xl rounded-2xl">
-        <CardHeader>
-          <CardTitle className="text-2xl text-center">Create Account</CardTitle>
-        </CardHeader>
+    <>
+      <Helmet>
+        <title>Signup</title>
+      </Helmet>
+      
+      <div className="flex items-center justify-center min-h-screen bg-muted">
+        <Card className="w-full max-w-md shadow-xl rounded-2xl">
+          <CardHeader>
+            <CardTitle className="text-2xl text-center">
+              Create Account
+            </CardTitle>
+          </CardHeader>
 
-        <CardContent>
-          <form className="space-y-4">
-            <div className="grid grid-cols-2 gap-3">
-              <div>
-                <Label className={"mb-2"}>First Name</Label>
-                <Input type={"text"} placeholder="Enter your first name" />
+          <CardContent>
+            <form className="space-y-4">
+              <div className="grid grid-cols-2 gap-3">
+                <div>
+                  <Label className={"mb-2"}>First Name</Label>
+                  <Input type={"text"} placeholder="Enter your first name" />
+                </div>
+
+                <div>
+                  <Label className={"mb-2"}>Last Name</Label>
+                  <Input type={"text"} placeholder="Enter your last name" />
+                </div>
               </div>
 
               <div>
-                <Label className={"mb-2"}>Last Name</Label>
-                <Input type={"text"} placeholder="Enter your last name" />
+                <Label className={"mb-2"}>Email</Label>
+                <Input type="email" placeholder="Enter your email" />
               </div>
-            </div>
 
-            <div>
-              <Label className={"mb-2"}>Email</Label>
-              <Input type="email" placeholder="Enter your email" />
-            </div>
+              <div>
+                <Label className={"mb-2"}>Password</Label>
+                <Input type="password" placeholder="*********" />
+              </div>
 
-            <div>
-              <Label className={"mb-2"}>Password</Label>
-              <Input type="password" placeholder="*********" />
-            </div>
-
-            <Button className="w-full mt-2 cursor-pointer">Sign Up</Button>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
+              <Button className="w-full mt-2 cursor-pointer">Sign Up</Button>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 };
 
