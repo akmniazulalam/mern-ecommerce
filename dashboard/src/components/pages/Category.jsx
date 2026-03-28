205a2e6feb89768eff2f1b4df6e16af552cd2fef
akmniazulalam <niazulalam097@gmail.com>
2026-03-28 13:50:37 +0600

    document title change using react-helmet-async

--- dashboard/src/components/pages/Category.jsx
+++ dashboard/src/components/pages/Category.jsx
@@ -5,44 +5,58 @@ import { Textarea } from "@/components/ui/textarea";
 import axios from "axios";
 import { useState } from "react";
 import { Helmet } from "react-helmet-async";
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 const Category = () => {
-  const [categoryName, setCategoryName] = useState("")
-  const [categoryDescription, setCategoryDescription] = useState("")
-  const navigate = useNavigate()
+  const [categoryName, setCategoryName] = useState("");
+  const [categoryDescription, setCategoryDescription] = useState("");
+  const navigate = useNavigate();
   const formData = {
     name: categoryName,
-    description: categoryDescription
-  }
+    description: categoryDescription,
+  };
   const handleCreateCategory = () => {
-    axios.post("https://mern-ecommerce-91cv.onrender.com/api/v1/category/createcategory", formData)
-    toast.success('Successfully added!');
-    setCategoryName("")
-    setCategoryDescription("")
-    setTimeout(()=> {
-      navigate("/categorylist")
-    }, 1000)
-  }
+    axios.post(
+      "https://mern-ecommerce-91cv.onrender.com/api/v1/category/createcategory",
+      formData,
+    );
+    toast.success("Successfully added!");
+    setCategoryName("");
+    setCategoryDescription("");
+    setTimeout(() => {
+      navigate("/categorylist");
+    }, 1000);
+  };
   return (
     <>
-    <Helmet>
-      <title>Add Category</title>
-    </Helmet>
+      <Helmet>
+        <title>Add Category</title>
+      </Helmet>
       <h3 className="font-bold">Add Category</h3>
       <div className="max-w-1/3 mt-4">
         <FieldGroup>
           <Field>
             <FieldLabel>Category Name</FieldLabel>
-            <Input value={categoryName} placeholder="Category Name" onChange={(e) => setCategoryName(e.target.value)}/>
+            <Input
+              value={categoryName}
+              placeholder="Category Name"
+              onChange={(e) => setCategoryName(e.target.value)}
+            />
           </Field>
           <Field>
             <FieldLabel>Category Description</FieldLabel>
-            <Textarea value={categoryDescription} placeholder="Type your description here..." className={"resize-none"} onChange={(e) => setCategoryDescription(e.target.value)} />
+            <Textarea
+              value={categoryDescription}
+              placeholder="Type your description here..."
+              className={"resize-none"}
+              onChange={(e) => setCategoryDescription(e.target.value)}
+            />
           </Field>
           <Field orientation="horizontal">
-            <Button onClick={handleCreateCategory} className={"cursor-pointer"}>Add Category</Button>
+            <Button onClick={handleCreateCategory} className={"cursor-pointer"}>
+              Add Category
+            </Button>
           </Field>
         </FieldGroup>
       </div>
