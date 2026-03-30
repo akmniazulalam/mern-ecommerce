35a7f68753989f2570d5c8cb530cdf104b416017
akmniazulalam <niazulalam097@gmail.com>
2026-03-30 16:41:41 +0600

    Globally dark mode apply

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -14,19 +14,17 @@ import AuthLayout from "./components/layout/AuthLayout";
 import Signup from "./components/pages/Signup";
 import Login from "./components/pages/Login";
 import VerifyOtp from "./components/pages/VerifyOtp";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  
   useEffect(() => {
     const savedTheme = localStorage.getItem("theme");
 
     if (savedTheme === "dark") {
       document.documentElement.classList.add("dark");  //document.documentElement = <html> tag
-      setDarkMode(true);
     } else {
       document.documentElement.classList.remove("dark");
-      setDarkMode(false);
     }
   }, []);
   return (
