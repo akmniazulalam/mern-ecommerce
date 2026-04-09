bef8879d5f3d2d440271221f5c34c1f74a487cad
akmniazulalam <niazulalam097@gmail.com>
2026-04-09 12:57:06 +0600

    button width fix for mobile responsive

--- dashboard/src/components/StatsCard.jsx
+++ dashboard/src/components/StatsCard.jsx
@@ -2,15 +2,15 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 const StatsCard = ({title, value}) => {
   return (
-    <Card className={"py-3 md:py-6 gap-1 md:gap-6"}>
+    <Card className={"py-4 md:py-6 gap-2 md:gap-6"}>
       <CardHeader >
-        <CardTitle className="text-sm text-muted-foreground">
+        <CardTitle className="text-muted-foreground">
           {/* <h3>gggg</h3> */}
           {title}
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="text-base md:text-2xl font-bold">
+        <div className="font-bold">
           {/* <h3>ggggggggg</h3> */}
           {value}
         </div>
