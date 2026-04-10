3f05cdbf71cd380b6e8723ae269b68a5ac6a7a26
akmniazulalam <niazulalam097@gmail.com>
2026-04-11 00:10:47 +0600

    cors origin and credential add

--- ecommerceApi/src/app.js
+++ ecommerceApi/src/app.js
@@ -1,24 +1,31 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
-const path = require('path');
-const session = require("express-session")
-const routes = require('./routes')
-app.use(express.json())
-app.use(cors())
-app.use(session({
-  secret: 'ecommerceApi',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false }
-}))
+const cors = require("cors");
+const path = require("path");
+const session = require("express-session");
+const routes = require("./routes");
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+app.use(
+  session({
+    secret: "ecommerceApi",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  }),
+);
 
-app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')))
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.use('/api/v1', routes)
+app.use("/api/v1", routes);
 
-module.exports = app
+module.exports = app;
