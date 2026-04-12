f66c7a100855e3c84698eb342890d1a908fc129a
akmniazulalam <niazulalam097@gmail.com>
2026-04-12 11:41:17 +0600

    Update CORS to allow Vercel frontend origin with credentials also

--- ecommerceApi/src/app.js
+++ ecommerceApi/src/app.js
@@ -7,7 +7,7 @@ const routes = require("./routes");
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://mern-ecommerce-sigma-nine.vercel.app"],
     credentials: true,
   }),
 );
