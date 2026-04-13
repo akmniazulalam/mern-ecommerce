5ddf715dd88643fe5c5c0a4285c31b41d52226d8
akmniazulalam <niazulalam097@gmail.com>
2026-04-14 01:36:55 +0600

    async await use for fetching current user

--- dashboard/src/components/pages/Dashboard.jsx
+++ dashboard/src/components/pages/Dashboard.jsx
@@ -1,22 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import StatsCard from '../StatsCard'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import StatsCard from "../StatsCard";
+import axios from "axios";
 
 const Dashboard = () => {
-
   const [currentUser, setCurrentUser] = useState(null);
 
-  useEffect(async () => {
-    await axios
-      .get("https://mern-ecommerce-91cv.onrender.com/api/v1/auth/currentuser", {
-        withCredentials: true,
-      })
-      .then((res) => setCurrentUser(res.data.user));
+  useEffect(() => {
+    const user = async () => {
+      await axios
+        .get(
+          "https://mern-ecommerce-91cv.onrender.com/api/v1/auth/currentuser",
+          {
+            withCredentials: true,
+          },
+        )
+        .then((res) => setCurrentUser(res.data.user));
+    };
+    user();
   }, []);
 
   return (
     <div className="space-y-8">
-      <h2 className='text-4xl font-bold'>Welcome {currentUser?.firstName} {currentUser?.lastName}</h2>
+      <h2 className="text-4xl font-bold">
+        Welcome {currentUser?.firstName} {currentUser?.lastName}
+      </h2>
       <div className="grid md:grid-cols-4 gap-6">
         <StatsCard title="Total Revenue" value="$45,231" />
         <StatsCard title="Orders" value="1,350" />
@@ -24,7 +31,7 @@ const Dashboard = () => {
         <StatsCard title="Products" value="120" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
