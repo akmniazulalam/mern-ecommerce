3721c18714a679bc93c1fb9f63b588a11a82a301
akmniazulalam <niazulalam097@gmail.com>
2026-04-16 02:36:47 +0600

    add avatar upload with cloudinary integration

--- ecommerceApi/src/modules/auth/auth.routes.js
+++ ecommerceApi/src/modules/auth/auth.routes.js
@@ -1,4 +1,16 @@
 const express = require("express");
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split(".")[1])
+  }
+})
+
+const upload = multer({ storage: storage })
 const {
   signupController,
   getAllUsers,
