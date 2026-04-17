4e36d7fc2d000ee5edfd67ba7d2df355a177973a
akmniazulalam <niazulalam097@gmail.com>
2026-04-18 00:32:09 +0600

    protected route component add

---
+++ dashboard/src/components/ProtectedRoute.jsx
@@ -0,0 +1,35 @@
+import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Loader2 } from "lucide-react";
+
+const ProtectedRoute = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("/currentuser", { withCredentials: true })
+      .then((res) => {
+        setUser(res.data.user);
+      })
+      .catch(() => {
+        setUser(null);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader2 className="animate-spin h-8 w-8" />
+      </div>
+    );
+  }
+
+  if (!user) return <Navigate to="/login" />;
+
+  return children;
+};
+
+export default ProtectedRoute;
