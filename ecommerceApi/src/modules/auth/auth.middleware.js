2a66d533eb71f9652e3ec7532b1bdae3c02c62a7
akmniazulalam <niazulalam097@gmail.com>
2026-04-17 23:48:40 +0600

    authMiddleware change

--- ecommerceApi/src/modules/auth/auth.middleware.js
+++ ecommerceApi/src/modules/auth/auth.middleware.js
@@ -1,6 +1,6 @@
 
 function authMiddleware(req, res, next) {
-  if (req.session.isAuth) {
+  if (req.session.user) {
     return next();
   } else {
     return res.status(401).json({ message: "Unauthorized access" });
