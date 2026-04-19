32e4da65324f8e2ea42f0920204fe639242f4997
akmniazulalam <niazulalam097@gmail.com>
2026-04-19 18:40:59 +0600

    page title change

--- dashboard/src/components/pages/Dashboard.jsx
+++ dashboard/src/components/pages/Dashboard.jsx
@@ -5,25 +5,25 @@ import { useAuth } from "@/context/AuthContext";
 import { Helmet } from "react-helmet-async";
 
 const Dashboard = () => {
-
-  const {user} = useAuth()
+  const { user } = useAuth();
 
   return (
     <>
-    <Helmet>
-      <title>Dashboard</title>
-    </Helmet>
+      <Helmet>
+        <title>Dashboard</title>
+      </Helmet>
+      
       <div className="space-y-8">
-      <h2 className="md:text-4xl font-bold text-xl">
-        Welcome {user?.firstName} {user?.lastName} to Dashboard
-      </h2>
-      <div className="grid md:grid-cols-4 gap-6 md:text-2xl">
-        <StatsCard title="Total Revenue" value="$45,231" />
-        <StatsCard title="Orders" value="1,350" />
-        <StatsCard title="Customers" value="820" />
-        <StatsCard title="Products" value="120" />
+        <h2 className="md:text-4xl font-bold text-xl">
+          Welcome {user?.firstName} {user?.lastName} to Dashboard
+        </h2>
+        <div className="grid md:grid-cols-4 gap-6 md:text-2xl">
+          <StatsCard title="Total Revenue" value="$45,231" />
+          <StatsCard title="Orders" value="1,350" />
+          <StatsCard title="Customers" value="820" />
+          <StatsCard title="Products" value="120" />
+        </div>
       </div>
-    </div> 
     </>
   );
 };
