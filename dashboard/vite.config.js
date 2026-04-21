eca48cefbaf790f82cbd306d79e30d288777b609
akmniazulalam <niazulalam097@gmail.com>
2026-04-21 23:59:46 +0600

    code format

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
