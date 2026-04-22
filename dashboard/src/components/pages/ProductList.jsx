07ec16ee60a553afdf11e64888c389dac0b470db
akmniazulalam <niazulalam097@gmail.com>
2026-04-22 23:55:18 +0600

    unnecessary import delete

--- dashboard/src/components/pages/ProductList.jsx
+++ dashboard/src/components/pages/ProductList.jsx
@@ -20,8 +20,6 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
-import { Pencil, Trash } from "lucide-react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
