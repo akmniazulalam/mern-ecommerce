e46472609fe1ae7c05fb43123130ee36dbad2ac3
akmniazulalam <niazulalam097@gmail.com>
2026-05-03 23:59:21 +0600

    add to cart add

--- ecommerceApi/src/modules/cart/cart.controller.js
+++ ecommerceApi/src/modules/cart/cart.controller.js
@@ -3,6 +3,27 @@ const cartModel = require("./cart.model")
 const addToCartController = async (req, res) => {
     const {userId, product} = req.body
     const cartItems = await cartModel.findOne({userId})
+
+    if (!cart) {
+    cart = new Cart({
+      userId,
+      items: [{ ...product, quantity: 1 }],
+    });
+  } else {
+    const existingItem = cart.items.find(
+      (item) => item.productId === product._id
+    );
+
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      cart.items.push({ ...product, quantity: 1 });
+    }
+  }
+
+  await cart.save();
+
+  res.json(cart);
 }
 
 module.exports = {addToCartController}
