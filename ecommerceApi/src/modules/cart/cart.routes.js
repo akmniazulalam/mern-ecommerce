ef153162a8576a55d59ad9a8802e2fae9bc8770c
akmniazulalam <niazulalam097@gmail.com>
2026-05-04 23:51:34 +0600

    get all cart item route create

--- ecommerceApi/src/modules/cart/cart.routes.js
+++ ecommerceApi/src/modules/cart/cart.routes.js
@@ -1,7 +1,8 @@
 const express = require("express")
-const { addToCartController } = require("./cart.controller")
+const { addToCartController, getAllCartItem } = require("./cart.controller")
 const router = express.Router()
 
 router.post("/addtocart", addToCartController)
+router.get("/allcart", getAllCartItem)
 
 module.exports = router
