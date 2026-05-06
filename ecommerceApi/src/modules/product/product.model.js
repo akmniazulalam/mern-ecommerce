09ffa1e9cf8ca76b368fba7cad9ba2c1994e6abd
akmniazulalam <niazulalam097@gmail.com>
2026-05-06 23:49:17 +0600

    variants add

--- ecommerceApi/src/modules/product/product.model.js
+++ ecommerceApi/src/modules/product/product.model.js
@@ -1,47 +1,52 @@
-const mongoose = require("mongoose")
-const {Schema} = mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const productSchema = new Schema({
+const productSchema = new Schema(
+  {
     name: {
-        type: String,
-        trim: true,
-        unique: true,
-        required: true
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
     },
     description: {
-        type: String,
-        trim: true,
-        required: true
+      type: String,
+      trim: true,
+      required: true,
     },
     category: {
-        type: String,
-        trim: true,
+      type: String,
+      trim: true,
     },
-    image: {
-        type: String
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    size: {
-        type: String,
-        trim: true,
-    },
-    color: {
-        type: String,
-        trim: true,
-    },
-    ram: {
-        type: String,
-        trim: true,
-    },
-    storage: {
-        type: String,
-        trim: true,
-    },
-},
-{timestamps: true}
-)
+    variants: [
+      {
+        image: {
+          type: String,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        size: {
+          type: String,
+          trim: true,
+        },
+        color: {
+          type: String,
+          trim: true,
+        },
+        ram: {
+          type: String,
+          trim: true,
+        },
+        storage: {
+          type: String,
+          trim: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true },
+);
 
-module.exports = mongoose.model("Product", productSchema)
+module.exports = mongoose.model("Product", productSchema);
