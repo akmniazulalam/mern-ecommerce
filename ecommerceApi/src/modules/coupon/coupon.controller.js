5dc7b775ef290eca71624a95cac86609e100231e
akmniazulalam <niazulalam097@gmail.com>
2026-05-12 14:42:21 +0600

    coupon controller add

--- ecommerceApi/src/modules/coupon/coupon.controller.js
+++ ecommerceApi/src/modules/coupon/coupon.controller.js
@@ -1,5 +1,63 @@
-const applyCouponController = async (req, res) => {
-    
-}
+const couponSchema = require("./coupon.model")
 
-module.exports = applyCouponController
+const applyCouponController = async (req, res) => {
+  try {
+    const { code, subtotal } = req.body;
+
+    const coupon = await couponSchema.findOne({
+      code: code.toUpperCase(),
+    });
+
+    if (!coupon) {
+      return res.status(404).json({
+        success: false,
+        message: "Invalid coupon code",
+      });
+    }
+
+    if (!coupon.isActive) {
+      return res.status(400).json({
+        success: false,
+        message: "Coupon is inactive",
+      });
+    }
+
+    if (new Date() > coupon.expiryDate) {
+      return res.status(400).json({
+        success: false,
+        message: "Coupon expired",
+      });
+    }
+
+    if (subtotal < coupon.minPurchase) {
+      return res.status(400).json({
+        success: false,
+        message: `Minimum purchase $${coupon.minPurchase}`,
+      });
+    }
+
+    let discount = 0;
+
+    if (coupon.discountType === "percentage") {
+      discount = (subtotal * coupon.discountValue) / 100;
+    } else {
+      discount = coupon.discountValue;
+    }
+
+    const total = subtotal - discount;
+
+    res.status(200).json({
+      success: true,
+      discount,
+      total,
+      coupon,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = applyCouponController;
