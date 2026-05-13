10cee6fa147ae00284e495e615620a2137f30142
akmniazulalam <niazulalam097@gmail.com>
2026-05-13 23:36:51 +0600

    code format

--- ecommerceApi/src/modules/coupon/coupon.routes.js
+++ ecommerceApi/src/modules/coupon/coupon.routes.js
@@ -1,10 +1,15 @@
-const express = require("express")
-const {createCouponController, applyCouponController, getCoupons, deleteCoupon} = require("./coupon.controller")
-const router = express.Router()
+const express = require("express");
+const {
+  createCouponController,
+  applyCouponController,
+  getCoupons,
+  deleteCoupon,
+} = require("./coupon.controller");
+const router = express.Router();
 
-router.post("/create-coupon", createCouponController)
-router.post("/apply-coupon", applyCouponController)
-router.get("/couponlist", getCoupons)
-router.delete("/deletecoupon/:id", deleteCoupon)
+router.post("/create-coupon", createCouponController);
+router.post("/apply-coupon", applyCouponController);
+router.get("/couponlist", getCoupons);
+router.delete("/deletecoupon/:id", deleteCoupon);
 
-module.exports = router
+module.exports = router;
