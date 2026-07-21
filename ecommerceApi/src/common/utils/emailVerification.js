1770d20df1df950f938f4a57c93e6acb04dcfe22
akmniazulalam <niazulalam097@gmail.com>
2026-07-21 17:21:54 +0600

    nodemailer use for otp send

--- ecommerceApi/src/common/utils/emailVerification.js
+++ ecommerceApi/src/common/utils/emailVerification.js
@@ -4,33 +4,33 @@ const { getEnv } = require("../config/env");
 
 async function emailVerification(email, otp) {
 
-  // const transporter = nodemailer.createTransport({
-  //   host: "smtp.gmail.com",
-  //   port: 587,
-  //   secure: false, // Use true for port 465, false for port 587
-  //   family: 4,
-  //   auth: {
-  //     user: process.env.EMAIL_USER,
-  //     pass: process.env.EMAIL_PASS,
-  //   },
-  // });
-
-  //   const info = await transporter.sendMail({
-  //     from: `"Otp" <${process.env.EMAIL_USER}>`,
-  //     to: email,
-  //     subject: "Otp",
-  //     text: "Otp Verification", // Plain-text version of the message
-  //     html: `<h1>Your Otp is: ${otp}</h1>`, // HTML version of the message
-  //   });
-
-  const resend = new Resend(getEnv("RESEND_API_KEY"));
-
-  await resend.emails.send({
-    from: "Otp <onboarding@resend.dev>",
-    to: email,
-    subject: "Otp",
-    html: `<h1>Your Otp is: ${otp}</h1>`,
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // Use true for port 465, false for port 587
+    family: 4,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
   });
+
+    const info = await transporter.sendMail({
+      from: `"Otp" <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject: "Otp",
+      text: "Otp Verification", // Plain-text version of the message
+      html: `<h1>Your Otp is: ${otp}</h1>`, // HTML version of the message
+    });
+
+  // const resend = new Resend(getEnv("RESEND_API_KEY"));
+
+  // await resend.emails.send({
+  //   from: "Otp <onboarding@resend.dev>",
+  //   to: email,
+  //   subject: "Otp",
+  //   html: `<h1>Your Otp is: ${otp}</h1>`,
+  // });
 }
 
 module.exports = emailVerification;
